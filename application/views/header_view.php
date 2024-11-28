d413f55407f5af4c6d96b099db2b300cecea17f1
kevlar453 <central.jdp@gmail.com>
2024-11-28 16:18:55 +0700

    2024-11-28 16:18:55

--- application/views/header_view.php
+++ application/views/header_view.php
@@ -3,6 +3,26 @@
 <html>
 
 <head>
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-979FCK021T"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-979FCK021T');
+  </script>
+
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-WRKXT3Z4');</script>
+  <!-- End Google Tag Manager -->
+
+
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php echo $title_web; ?></title>
@@ -59,6 +79,13 @@
 </head>
 
 <body class="hold-transition skin-green-light sidebar-mini">
+
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WRKXT3Z4"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
+
   <div class="wrapper">
     <header class="main-header">
 
